2089f1af45f49e4e1ba9a38447c959434cf66e57
Stephen <stephenfauvergue@gmail.com>
2019-04-08 11:07:32 +0200

    create product page

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ProductsService} from '../../providers/products.service';
+import {CartService} from '../../providers/cart.service';
+import {MenuService} from '../../providers/menu.service';
 
 @Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.sass']
+    selector: 'app-product-page',
+    templateUrl: './product-page.component.html',
+    styleUrls: ['./product-page.component.sass']
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+    product: any = {};
 
-  ngOnInit() {
-  }
+    constructor(
+        public activatedRoute: ActivatedRoute,
+        public router: Router,
+        public products: ProductsService,
+        public cart: CartService,
+        public menu: MenuService
+    ) {
+
+        this.activatedRoute
+            .params
+
+            .subscribe(
+                params => {
+                    let id = params['id'];
+                    this.products.view(id)
+                        .then((product: any) => {
+                            this.product = product;
+                        })
+                        .catch((e) => {
+                            console.error(e);
+                            this.router.navigate(['/404']);
+                        });
+                });
+    }
+
+    ngOnInit() {
+    }
 
 }
